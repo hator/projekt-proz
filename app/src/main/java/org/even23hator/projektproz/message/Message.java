c359953955bc7d10476d2edb1ed61d715581157d
Jan Michalski <jan.michalski@buzytor.com>
2016-04-17 21:10:14 +0200

    Add message passing system

---
+++ app/src/main/java/org/even23hator/projektproz/message/Message.java
@@ -0,0 +1,8 @@
+package org.even23hator.projektproz.message;
+
+/**
+ * Created by hator on 17.04.16.
+ */
+public class Message {
+    public MessageType type;
+}
---
+++ app/src/main/java/org/even23hator/projektproz/message/MessageRouter.java
@@ -0,0 +1,41 @@
+package org.even23hator.projektproz.message;
+
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Observable;
+import java.util.Set;
+
+/**
+ * Created by hator on 17.04.16.
+ */
+public class MessageRouter extends Observable {
+    private static MessageRouter ourInstance = new MessageRouter();
+
+    public static MessageRouter getInstance() {
+        return ourInstance;
+    }
+
+
+    private EnumMap<MessageType,Set<IMessageListener>> listeners;
+
+    private MessageRouter() {
+    }
+
+    public void registerListener(IMessageListener listener, MessageType type) {
+        Set<IMessageListener> bucket = listeners.get(type);
+        if(bucket == null) {
+            bucket = new HashSet<>();
+            listeners.put(type, bucket);
+        }
+        bucket.add(listener);
+    }
+
+    // FIXME add way to unregisterListeners
+
+    public void routeMessage(Message message) {
+        for(IMessageListener listener : listeners.get(message.type)) {
+            listener.onMessage(message);
+        }
+    }
+
+}
