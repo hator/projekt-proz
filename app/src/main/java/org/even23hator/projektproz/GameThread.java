842ae7f6ea5020b7034f02d60e977ae4010e39a7
Jan Michalski <jan.michalski@buzytor.com>
2016-04-25 14:24:11 +0200

    Make game loop use fixed timestep, add fps cap

--- app/src/main/java/org/even23hator/projektproz/GameThread.java
+++ app/src/main/java/org/even23hator/projektproz/GameThread.java
@@ -3,14 +3,23 @@ package org.even23hator.projektproz;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 /**
  * Created by hator on 23.04.16.
  */
 public class GameThread extends Thread {
+    public static final int TARGET_FPS = 60;
+    public static final int TIMESTEP_NS = 1000000000 / TARGET_FPS;
+
     private volatile boolean running;
     private GameView gameView;
+    private long elapsedTime;
+    private double fps;
+    private long frames;
+    private long dt;
 
     public GameThread(GameView gameView) {
         super();
@@ -24,18 +33,43 @@ public class GameThread extends Thread {
 
     @Override
     public void run() {
-        long lastFrameTime = System.currentTimeMillis();
-        while(running) {
-            final long dt = lastFrameTime - System.currentTimeMillis();
-            lastFrameTime = System.currentTimeMillis();
+        long currentTime;
+        long startTime = SystemClock.elapsedRealtimeNanos();
+        long lastFrameTime = startTime;
+        dt = 0;
+        fps = 0;
+        frames = 0;
 
-            update(dt);
+        while (running) {
+            currentTime = SystemClock.elapsedRealtimeNanos();
+            elapsedTime = currentTime - lastFrameTime;
+            lastFrameTime = currentTime;
 
+            ++frames;
+            fps = (double)(frames) / (currentTime - startTime) * 1e9;
+
+            // NOTE(hator): for game mechanics update we use fixed timestep for simulation stability
+            //      we accumulate time in dt and use it up for updates
+            dt += elapsedTime;
+            Log.d("GameThread", "elapsed " + elapsedTime/1e6f + ", delta " + dt/1e6f);
+            while(dt >= TIMESTEP_NS) {
+                dt -= TIMESTEP_NS;
+                update(TIMESTEP_NS);
+            }
+
+            // NOTE(hator): drawing needs to be done by the UI thread so we post draw method to our view
             gameView.post(new Runnable() {
                 public void run() {
-                    draw(dt);
+                    draw();
                 }
             });
+
+            // NOTE(hator): if we render faster then target fps then we can sleep for a while
+            //      so we don't block the cpu (improves battery life etc.)
+            long sleepTime = (TIMESTEP_NS - dt) / 1000000;
+            if(sleepTime > 0) {
+                SystemClock.sleep(sleepTime);
+            }
         }
     }
 
@@ -44,7 +78,7 @@ public class GameThread extends Thread {
     }
 
 
-    private void draw(long dt) {
+    private void draw() {
         SurfaceHolder surfaceHolder = gameView.getHolder();
 
         synchronized (surfaceHolder) {
@@ -61,7 +95,8 @@ public class GameThread extends Thread {
             paint.setColor(Color.argb(255, 0, 0, 0));
             paint.setTextSize(45);
 
-            canvas.drawText("delta " + dt + "ms", 20, 40, paint);
+            canvas.drawText("delta " + dt/1000000.f + "ms", 20, 40, paint);
+            canvas.drawText("FPS " + fps, 20, 100, paint);
 
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
